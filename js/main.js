2081d5c03546e7f2b66436487045a14d9bc5383c
RimaKerirem <rima.kerirem@gmail.com>
2022-09-28 13:28:38 +0100

    Update

--- js/main.js
+++ js/main.js
@@ -2,9 +2,5 @@ let showPassword = document.getElementById("check");
 let password = document.getElementById("pass");
 
 showPassword.onclick = function () {
-  if (showPassword.checked) {
-    password.type = "text";
-  } else {
-    password.type = "password";
-  }
+  password.type = showPassword.checked ? "text" : "password";
 };
